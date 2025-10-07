4e7c59d089e3c3390df962b8a0f8690b0971d606
muhashi <105213357+muhashi@users.noreply.github.com>
2025-10-07 21:08:18 +1000

    Add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fakenewsdle/', // https://vitejs.dev/guide/static-deploy.html#github-pages
   plugins: [react()],
 })
